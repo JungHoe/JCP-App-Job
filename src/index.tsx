e984545118f53442675fa2ea04342c40f2715a6c
JungHoe <dlwjdghl159@naver.com>
2021-12-17 23:16:11 +0900

    docs (rm) : add ant design error on pnp

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React, { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-
+import 'antd/dist/antd.css'
 ReactDOM.render(
   <StrictMode>
     <App name="Coder###" />
